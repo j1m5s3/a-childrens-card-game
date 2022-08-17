88a16fddba3ce8457157881fdb8b1b4cda99c15b
j1m5s3 <james.lynch.uml@gmail.com>
2022-08-17 16:19:59 -0400

    Initial commit

---
+++ a-childrens-card-game/Assets/Scripts/EndTurn.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTurn : MonoBehaviour
+{
+    public GameObject PlayerOneCard;
+    public GameObject PlayerTwoCard;
+    public GameObject PlayerOneField;
+    public GameObject PlayerTwoField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   public void onBtnClick() 
+    {
+        var playerCard = Instantiate( this.PlayerOneCard, new Vector3( 0, 0, 0 ), Quaternion.identity );
+        playerCard.transform.SetParent( this.PlayerOneField.transform, false );
+    }
+}
